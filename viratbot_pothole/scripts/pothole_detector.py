7049a22792f7290de30d81a402feff767c88c630
Jnanneswara Rao Rompilli <jnaneshvsjnani@gmail.com>
2021-07-25 16:06:48 +0530

    Publish Pothole coords

--- viratbot_pothole/scripts/pothole_detector.py
+++ viratbot_pothole/scripts/pothole_detector.py
@@ -2,15 +2,18 @@
 
 import cv2 as cv
 import rospy
+import tf
+import tf2_ros
+import tf2_geometry_msgs
+from tf.transformations import euler_from_quaternion, quaternion_matrix, rotation_from_matrix
+from tf import TransformerROS
 import numpy as np
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Pose
+from nav_msgs.msg import Odometry
 from cv_bridge import CvBridge
 
-fx = 595.8788545
-fy = 595.8788545
-cx = 500.5
-cy = 400.5
-
+hgph = []
 
 class bcolors:
     HEADER = '\033[95m'
@@ -24,7 +27,29 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+def convert_frame(input_pose):
+    tf_buffer = tf2_ros.Buffer()
+    tf_listener = tf2_ros.TransformListener(tf_buffer)
+
+    pose_stamped = tf2_geometry_msgs.PoseStamped()
+
+    pose_stamped.pose = input_pose
+    pose_stamped.header.frame_id = 'camera_base'
+    pose_stamped.header.stamp = rospy.Time(0)
+
+    try:
+        # ** It is important to wait for the listener to start listening. Hence the rospy.Duration(1)
+        output_pose_stamped = tf_buffer.transform(
+            pose_stamped, 'odom', rospy.Duration(1))
+        return output_pose_stamped.pose
+
+    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+        raise
+
+
 def detect_potholes(cur_frame):
+    global hgph
+
     img = cv.cvtColor(cur_frame, cv.COLOR_BGR2GRAY)
     edged = cv.Canny(img, 30, 200)
     contours, _ = cv.findContours(
@@ -35,7 +60,7 @@ def detect_potholes(cur_frame):
     dim = []
     for contour in contours:
         (x, y, w, h) = cv.boundingRect(contour)
-        if cv.contourArea(contour) < 60000 and cv.contourArea(contour) > 100:
+        if cv.contourArea(contour) < 60000 and cv.contourArea(contour) > 10:
             cnt = cnt + 1
             cv.rectangle(cur_frame, (x, y), (x+w, y+h), (255, 0, 0), 1)
             center.append((x + w/2, y + h/2))
@@ -45,28 +70,60 @@ def detect_potholes(cur_frame):
         print(f"\n{bcolors.OKGREEN} Potholes Detected !")
         print(f"    {bcolors.OKCYAN}Count: {int(cnt)}{bcolors.ENDC}")
         for i in range(cnt):
-            print(f"    {i+1}-")
-            print(f"      Center: {center[i]}")
-            # x_w = (center[i][0]-cx)*0.54 / fx
-            # y_w = (center[i][1]-cy)*0.54 / f
-            # print(f"     WC: ({x_w},{y_w})")
+            print(f"    {i+1}:")
+            print(f"      Pixel Corord: {center[i]}")
 
-            print(f"      W,H: {dim[i]} ")
+            b = np.array([[center[i][0]], [center[i][1]], [1]])
+            x = np.dot(hgph, b)
+            x = x/x[2][0]
+            i_pose = Pose()
+            i_pose.position.x = x[0][0]
+            i_pose.position.y = x[1][0]
+            res = convert_frame(i_pose)
+
+            print(f"      (W Coord:  {res.position.x},{res.position.y})")
 
     cv.imshow("World", cur_frame)
     cv.waitKey(500)
 
 
-def callback(data):
+def update_image(data):
     br = CvBridge()
     cur_frame = br.imgmsg_to_cv2(data)
     detect_potholes(cur_frame)
 
+def map_coord():
+    global hgph
+    pts_src = np.array([
+        [1, 800],
+        [1000, 800],
+        [1000, 43],
+        [1, 43],
+        [500, 203],
+        [500, 389.5],
+        [501, 105.5],
+        [686.5, 105.5],
+        [306.5, 105]])
+    pts_dst = np.array([
+        [0.1987, 0.3946],
+        [0.1987, -0.3946],
+        [5.9334, -4.2794],
+        [5.9334, 4.2794],
+        [1.6845, -0.001],
+        [0.769, -0.001],
+        [3.1148, -0.001],
+        [3.1336, -0.8928],
+        [3.1336, 0.9235]])
+
+    hgph, status = cv.findHomography(pts_src, pts_dst)
+
+
 
 def recieve_image():
     rospy.init_node('Pothole_detector', anonymous=True)
-    rospy.Subscriber('/viratbot/camera1/image_raw', Image, callback)
-    rate = rospy.Rate(2)
+    map_coord()
+
+    rospy.Subscriber('/viratbot/camera1/image_raw', Image, update_image)
 
     rospy.spin()
 
